b88302a6dfdd69071ec2e5685a77e32eb5ed0896
shagent007 <shagent118@gmail.com>
2022-03-21 20:25:18 +0600

    вапр

--- main.js
+++ main.js
@@ -1,62 +1,52 @@
 window.addEventListener("DOMContentLoaded", function() {
-  var canvas = document.getElementById("renderCanvas");
-  var engine = new BABYLON.Engine(canvas, true);
-  var createScene = function() {
-    var scene = new BABYLON.Scene(engine);
+  const canvas = document.getElementById("renderCanvas");
+  const engine = new BABYLON.Engine(canvas, true);
+  
+  const createScene = () => {
+    const scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color3(1, 1, 1);
-    var camera = new BABYLON.ArcRotateCamera(
-      "camera1",
+    
+    new BABYLON.ArcRotateCamera(
+      "",
       0,
       Math.PI / 2,
       1000,
       new BABYLON.Vector3.Zero(),
       scene
-    );
-    camera.attachControl(canvas, false);
-    var light1 = new BABYLON.HemisphericLight(
-      "light1",
+    ).attachControl(canvas, false);
+
+    new BABYLON.HemisphericLight(
+      "",
       new BABYLON.Vector3(0, 1, 0),
       scene
-    );
-    var light2 = new BABYLON.HemisphericLight(
-      "light1",
+    ).intensity = 0.7;
+
+    new BABYLON.HemisphericLight(
+      "",
       new BABYLON.Vector3(0, -1, 0),
       scene
-    );
-    light1.intensity = 0.7;
-    light2.intensity = 0.2;
+    ).intensity = 0.2;
 
-    var red = new Array(0.7, 0.1, 0.1, 0.7, 0.1);
-    var green = new Array(0.1, 0.7, 0.1, 0.7, 0.7);
-    var blue = new Array(0.1, 0.1, 0.7, 0.1, 0.7);
+    const n = 4;
+    const rl = 0.5;
+    const control = {}
+    const gui = new dat.GUI({ closeOnTop: true, width: 190 });  
+    let previousLevel = -1;
+    const controlElements = [];
+    const spheres = [];
 
-    function circle(r) {
-      var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, r, scene);
-      var materials = new BABYLON.StandardMaterial("mat", scene);
-      sphere.material = materials;
-      return sphere;
+    const createControls = (sphere, level) => {
+      spheres.push({sphere, level});
+      if(level<=previousLevel) return;
+      control[`group_${level}`] = true;
+      controlElements.push({
+        control:gui.add(control, `group_${level}`),
+        level});
+      previousLevel = level;
     }
 
-    var n = 4;
-    var rl = 0.5;
-    var sph = new Array();
-
-    function fractal(x, y, z, r, l, v) {
-      var parametrs = new (function() {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.r = r;
-        this.l = l;
-        this.shar = new circle(this.r);
-        this.shar.position = new BABYLON.Vector3(this.x, this.y, this.z);
-        this.shar.material.diffuseColor = new BABYLON.Color3(
-          red[l],
-          green[l],
-          blue[l]
-        );
-      })();
-
+    (function fractal(x, y, z, r, l, v) {
+      createControls(createSphere(x, y, z, r, l), l);
       if (l < n) {
         if (v != 2) fractal(x + (r / 2) * (rl + 1), y, z, r * rl, l + 1, 1);
         if (v != 1) fractal(x - (r / 2) * (rl + 1), y, z, r * rl, l + 1, 2);
@@ -65,50 +55,16 @@ window.addEventListener("DOMContentLoaded", function() {
         if (v != 6) fractal(x, y, z + (r / 2) * (rl + 1), r * rl, l + 1, 5);
         if (v != 5) fractal(x, y, z - (r / 2) * (rl + 1), r * rl, l + 1, 6);
       }
+    })(0, 0, 0, 256, 0, 0);
+  
+    for (const {control,level} of controlElements) {
+      control.onChange((val)=>controlSphere(val,level));
+    } 
 
-      sph.push(parametrs);
-    }
-
-    fractal(0, 0, 0, 256, 0, 0);
-
-    var InputGUIa = function() {
-      this.group_1 = true;
-      this.group_2 = true;
-      this.group_3 = true;
-      this.group_4 = true;
-      this.group_5 = true;
-    };
-
-    var InputGUI = new InputGUIa();
-    var gui = new dat.GUI({ closeOnTop: true, width: 190 });
-
-    var check1 = gui.add(InputGUI, "group_1");
-    var check2 = gui.add(InputGUI, "group_2");
-    var check3 = gui.add(InputGUI, "group_3");
-    var check4 = gui.add(InputGUI, "group_4");
-    var check5 = gui.add(InputGUI, "group_5");
-
-    check1.onChange(function(value) {
-      view(value, 0);
-    });
-    check2.onChange(function(value) {
-      view(value, 1);
-    });
-    check3.onChange(function(value) {
-      view(value, 2);
-    });
-    check4.onChange(function(value) {
-      view(value, 3);
-    });
-    check5.onChange(function(value) {
-      view(value, 4);
-    });
-
-    function view(v, lv) {
-      for (var i = 0; i < sph.length; i++) {
-        if (sph[i].l == lv) {
-          sph[i].shar.visibility = v;
-        }
+    const controlSphere = (controlValue,controlLevel) => {
+      for (const {sphere, level:sphereLevel} of spheres) {
+        if(sphereLevel !== controlLevel) continue;
+        sphere.visibility = controlValue;
       }
     }
 
@@ -117,11 +73,19 @@ window.addEventListener("DOMContentLoaded", function() {
 
   var scene = createScene();
 
-  engine.runRenderLoop(function() {
-    scene.render();
-  });
-
-  window.addEventListener("resize", function() {
-    engine.resize();
-  });
+  engine.runRenderLoop(() => scene.render());
+  window.addEventListener("resize",()=>  engine.resize());
 });
+
+
+var red = new Array(0.7, 0.1, 0.1, 0.7, 0.1);
+var green = new Array(0.1, 0.7, 0.1, 0.7, 0.7);
+var blue = new Array(0.1, 0.1, 0.7, 0.1, 0.7);
+
+const createSphere = (x, y, z, r, l) => {
+  const sphere = new BABYLON.Mesh.CreateSphere("sphere1", 16, r, scene);
+  sphere.material = new BABYLON.StandardMaterial("mat", scene);
+  sphere.position = new BABYLON.Vector3(x, y, z);
+  sphere.material.diffuseColor = new BABYLON.Color3(red[l], green[l],  blue[l]);
+  return sphere;
+}
